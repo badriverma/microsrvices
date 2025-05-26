59e131bd51d223931ada785cb30b81d695f77461
badriverma <badriverma135@gmail.com>
2025-05-26 23:28:37 +0530

    implementing spring profile configuration

---
+++ section6/v1-spring-cloud-config/loans/src/main/java/com/loans/dto/LoansContactInfoDto.java
@@ -0,0 +1,9 @@
+package com.loans.dto;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.*;
+
+@ConfigurationProperties(prefix = "loans")
+public record LoansContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+}
