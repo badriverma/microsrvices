fe8a8a6daf6c2e9b23f3369e61139ccd297dcd96
badriverma <badriverma135@gmail.com>
2025-05-26 23:06:58 +0530

    implementing spring profile configuration

--- section6/v1-springboot/cards/src/main/java/com/cards/dto/CardsContactInfoDto.java
+++ section6/v1-springboot/cards/src/main/java/com/cards/dto/CardsContactInfoDto.java
@@ -1,4 +1,10 @@
 package com.cards.dto;
 
-public class CardsContactInfoDto {
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
+import java.util.Map;
+
+@ConfigurationProperties(prefix = "cards")
+public record CardsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport){
 }
